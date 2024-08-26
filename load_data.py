4c237549a2daf00b46f264ad16489d28d74961d6
typhnz <tyvdphsn@gmail.com>
2024-08-26 12:32:52 +0200

    add load_data to my branch

---
+++ load_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# load the dataset
+df = pd.read_csv('bitcoin_sample_data.csv')
+
+# display the first few rows of the dataset
+print(df.head())
+
+# calculate the average (mean), median, and standard deviation of Bitcoin prices.
